407d6acf56eb8fe540b75999126938bb556b7906
kenen <01172014@pw.edu.pl>
2024-08-10 22:41:07 +0200

    task2 documentation

--- Task2/calculator_assignment/calculator.py
+++ Task2/calculator_assignment/calculator.py
@@ -1,63 +1,93 @@
 import decimal
 from decimal import Decimal as D
-from typing import Union
+from typing import Union, Callable
+
 
 class Calculator:
+    """
+    A class for performing basic arithmetic operations
+    :param value: Result of calculations
+    """
+
+    # changing all float and int arguments to decimal for more accurate representation of the data
     def __init__(self, value: Union[int, float] = D("0")) -> None:
+        """
+        Initializes the calculator with a starting value
+        :param value: The initial value of the calculator, later result of the calculations
+        """
         try:
             self.value = D(str(value))
         except decimal.InvalidOperation:
-            print("The value must be a number. The value has been set to zero")
+            print("The value must be a number. The value has been set to zero.")
             self.value = D("0")
         except Exception as e:
             print(f"Unexpected error: {e}")
             self.value = D("0")
 
+    @staticmethod
+    def arithmetic_operation(func: Callable) -> Callable:
+        """
+        Decorator responsible for handling exceptions from all functions
+        :param func: The decorated function
+        :return: The decorated function with exception handling
+        """
+
+        def wrapper(self, number: Union[int, float]) -> None:
+            try:
+                func(self, number)
+            except ZeroDivisionError:
+                print("You cannot divide by zero. The value has been reset to zero.")
+                self.reset_calculator()
+            except decimal.InvalidOperation:
+                print("The value must be a number. The value has been reset to zero.")
+                self.reset_calculator()
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                self.reset_calculator()
+
+        return wrapper
+
+    @arithmetic_operation
     def add(self, number: Union[int, float]) -> None:
-        try:
-            self.value += D(str(number))
-        except decimal.InvalidOperation:
-            print("The value must be a number. The value has been reset to zero")
-            self.reset_calculator()
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            self.reset_calculator()
+        """
+        Adds a number to the current value
+        :param number: The added number
+        """
+        self.value += D(str(number))
 
+    @arithmetic_operation
     def subtract(self, number: Union[int, float]) -> None:
-        try:
-            self.value -= D(str(number))
-        except decimal.InvalidOperation:
-            print("The value must be a number. The value has been reset to zero")
-            self.reset_calculator()
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            self.reset_calculator()
+        """
+        Subtracts a number from the current value
+        :param number: The number to subtract
+        """
+        self.value -= D(str(number))
 
+    @arithmetic_operation
     def multiply(self, number: Union[int, float]) -> None:
-        try:
-            self.value *= D(str(number))
-        except decimal.InvalidOperation:
-            print("The value must be a number. The value has been reset to zero")
-            self.reset_calculator()
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            self.reset_calculator()
+        """
+        Multiplies the current value by a number
+        :param number: The number to multiply by
+        """
+        self.value *= D(str(number))
 
+    @arithmetic_operation
     def divide(self, number: Union[int, float]) -> None:
-        try:
-            self.value /= D(str(number))
-        except ZeroDivisionError:
-            print("You cannot divide by zero. The value has been reset to zero")
-            self.reset_calculator()
-        except decimal.InvalidOperation:
-            print("The value must be a number. The value has been reset to zero")
-            self.reset_calculator()
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            self.reset_calculator()
+        """
+        Divides the current value by a number
+        :param number: The number to divide by
+        """
+        self.value /= D(str(number))
 
     def get_value(self) -> float:
+        """
+        Returns the current value of the calculator as a float
+        :return:  Float of the current value
+        """
         return float(self.value)
 
     def reset_calculator(self) -> None:
+        """
+        Resets calculator by changing its value to zero
+        """
         self.value = D("0")
--- Task2/calculator_assignment/calculator_test.py
+++ Task2/calculator_assignment/calculator_test.py
@@ -2,28 +2,50 @@ import pytest
 from calculator import Calculator
 
 
+# setting up calculators with different values
 @pytest.fixture
 def calculator_positive_starting_value():
+    """
+    Fixture providing Calculator object with starting value equal to two
+    """
     return Calculator(2)
 
 
 @pytest.fixture
 def calculator_negative_starting_value():
+    """
+    Fixture providing Calculator object with starting value equal to minus two
+    """
     return Calculator(-2)
 
 
+# setting up calculator with value=0
 @pytest.fixture
 def calculator_0_starting_value():
+    """
+    Fixture providing Calculator object with starting value equal to zero
+    """
     return Calculator()
 
 
+# Test parameters:
+# - input_n: The number used in the operation.
+# - expected_a: The expected result of the operation for the Calculator(2)
+# - expected_b: The expected result of the operation for the Calculator()
+# - expected_c: The expected result of the operation for the Calculator(-2)
+
+
+# parametrizing to test more data combinations
 @pytest.mark.parametrize(
+    # 3 expected values for calculators with different starting values
     ('input_n', 'expected_a', 'expected_b', 'expected_c'),
     [
+        # testing positive, negative and equal to zero ints
         (5, 7, 5, 3),
         (-5, -3, -5, -7),
-        (0, 2, 0, -2),  # 1
+        (0, 2, 0, -2),
 
+        # testing positive and negative floats
         (5.1, 7.1, 5.1, 3.1),
         (-5.2, -3.2, -5.2, -7.2),
 
@@ -31,11 +53,14 @@ def calculator_0_starting_value():
 )
 def test_addition(calculator_positive_starting_value, calculator_0_starting_value, calculator_negative_starting_value,
                   input_n, expected_a, expected_b, expected_c):
-    # def test_addition(calculator, params1):
-    # input_n, expected = params1
+    """
+    Tests the add method by adding a singular value
+    """
+
     calculator_positive_starting_value.add(input_n)
     calculator_0_starting_value.add(input_n)
     calculator_negative_starting_value.add(input_n)
+
     assert calculator_positive_starting_value.get_value() == expected_a
     assert calculator_0_starting_value.get_value() == expected_b
     assert calculator_negative_starting_value.get_value() == expected_c
@@ -44,17 +69,22 @@ def test_addition(calculator_positive_starting_value, calculator_0_starting_valu
 @pytest.mark.parametrize(
     ('input_n', 'expected_a', 'expected_b', 'expected_c'),
     [
+        # testing ints
         (10, -8, -10, -12),
         (-5, 7, 5, 3),
-        (0, 2, 0, -2),  # 1
+        (0, 2, 0, -2),
 
+        # testing floats
         (10.1, -8.1, -10.1, -12.1),
         (-5.2, 7.2, 5.2, 3.2),
 
     ]
 )
-def test_substract(calculator_positive_starting_value, calculator_0_starting_value, calculator_negative_starting_value,
-                   input_n, expected_a, expected_b, expected_c):
+def test_subtract(calculator_positive_starting_value, calculator_0_starting_value, calculator_negative_starting_value,
+                  input_n, expected_a, expected_b, expected_c):
+    """
+    Tests the subtract method by subtracting a singular value
+    """
     calculator_positive_starting_value.subtract(input_n)
     calculator_0_starting_value.subtract(input_n)
     calculator_negative_starting_value.subtract(input_n)
@@ -66,19 +96,25 @@ def test_substract(calculator_positive_starting_value, calculator_0_starting_val
 @pytest.mark.parametrize(
     ('input_n', 'expected_a', 'expected_b', 'expected_c'),
     [
+        # testing ints
         (4, 8, 0, -8),
         (-2, -4, 0, 4),
         (0, 0, 0, 0),  # 1
 
+        # testing floats
         (4.1, 8.2, 0, -8.2),
         (-2.4, -4.8, 0, 4.8),
     ]
 )
 def test_multiply(calculator_positive_starting_value, calculator_0_starting_value, calculator_negative_starting_value,
                   input_n, expected_a, expected_b, expected_c):
+    """
+    Tests the multiply method by multiplying initial value once
+    """
     calculator_positive_starting_value.multiply(input_n)
     calculator_0_starting_value.multiply(input_n)
     calculator_negative_starting_value.multiply(input_n)
+    # different initial values change the sign of a resultr
     assert calculator_positive_starting_value.get_value() == expected_a
     assert calculator_0_starting_value.get_value() == expected_b
     assert calculator_negative_starting_value.get_value() == expected_c
@@ -88,7 +124,7 @@ def test_multiply(calculator_positive_starting_value, calculator_0_starting_valu
     ('input_n', 'expected_a', 'expected_b', 'expected_c'),
     [
         (2, 1, 0, -1),
-        (-2, -1, 0, 1),  # 1
+        (-2, -1, 0, 1),
 
         (8, 0.25, 0, -0.25),
         (-10, -0.2, 0, 0.2)
@@ -96,6 +132,9 @@ def test_multiply(calculator_positive_starting_value, calculator_0_starting_valu
 )
 def test_divide(calculator_positive_starting_value, calculator_0_starting_value, calculator_negative_starting_value,
                 input_n, expected_a, expected_b, expected_c):
+    """
+    Tests the divide method by dividing initial value once
+    """
     calculator_positive_starting_value.divide(input_n)
     calculator_0_starting_value.divide(input_n)
     calculator_negative_starting_value.divide(input_n)
@@ -103,20 +142,31 @@ def test_divide(calculator_positive_starting_value, calculator_0_starting_value,
     assert calculator_0_starting_value.get_value() == expected_b
     assert calculator_negative_starting_value.get_value() == expected_c
 
-
 def test_reset(calculator_positive_starting_value):
+    """
+    Tests the reset method if it zeroes out the value
+    """
     calculator_positive_starting_value.reset_calculator()
     assert calculator_positive_starting_value.get_value() == 0
 
 
 def test_division_by_0(calculator_positive_starting_value, capsys):
+    """
+    Tests handling attempts to divide by zero. The test verifies both the result of
+     the operation and the message displayed in the terminal
+    """
     calculator_positive_starting_value.divide(0)
+    #capturing output to stdout
     out, err = capsys.readouterr()
     assert out == "You cannot divide by zero. The value has been reset to zero\n"
     assert calculator_positive_starting_value.get_value() == 0
 
 
 def test_incorrect_types(calculator_positive_starting_value, capsys):
+    """
+    Tests handling attempts to use incorrect data types in different Calculator methods. The test verifies both the
+    result of the operation and the message displayed in the terminal
+    """
     incorrect_value = "abc"
     calculator_positive_starting_value.divide(incorrect_value)
     out1, err1 = capsys.readouterr()
@@ -145,6 +195,10 @@ def test_incorrect_types(calculator_positive_starting_value, capsys):
 
 
 def test_chaining_multiple_operations(calculator_positive_starting_value):
+    """
+    Tests the accuracy of chaining multiple arithmetic operations together and
+     protection against float representation errors
+    """
     calculator_positive_starting_value.add(10)
     calculator_positive_starting_value.subtract(1123)
 
